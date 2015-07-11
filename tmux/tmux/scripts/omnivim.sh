fbe8ae63188a9ec926ffcbbabd90a2610c53136e
andy-lang <langy26@gmail.com>
2015-07-11 16:45:38 +0930

    Fixed omnivim issue where multiple windows would cause error output

--- tmux/tmux/scripts/omnivim.sh
+++ tmux/tmux/scripts/omnivim.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-#source ~/.bash_aliases
-
+# if you have a custom alias for vim (eg default flag commands), change this variable as necessary in order to maintain it.
 vim_command='gvim -v'
 
 # make sure we're in tmux. If we are...
@@ -9,7 +8,7 @@ if [ -n "$TMUX" ]; then
 	currentpaneid=$(tmux display-message -p "#D")
 	currentwindowid=$(tmux display-message -p "#{window_id}")
 	vimservercount=$($vim_command --serverlist | wc -l)
-	winname=${currentwindowid:1}
+	winname=${currentpaneid:1} # pane id but without the @
 	#echo "WINNAME = "$winname
 
 	#echo $currentpaneid, $currentwindowid, $vimservercount
@@ -26,28 +25,43 @@ if [ -n "$TMUX" ]; then
 		fi
 	
 	# there is a vim server open in the current window. So use it
-	elif [ "$vimservercount" -eq "1" ]; then
-		$vim_command --servername $vimservername --remote $@
+	#elif [ "$vimservercount" -eq "1" ]; then
+		#$vim_command --servername $vimservername --remote $@
 		##?? TODO switch to pane id of vim
 	
 	# we have multiple vim servers. We want the one that's in the current window.
 	# to do this we search the list of currently-running vim servers.
 	# if there exists a server with this window id, we have a match. Otherwise, open it in the current pane
 	else
-		for p in $(vim --serverlist); do
-			if [ "$p" -eq "$currentwindowid" ]; then
+		for p in $($vim_command --serverlist); do
+			if [ "$p" == "$currentwindowid" ]; then
+				echo "HERE"
 				$vim_command --servername $vimservername --remote $@
 				exit
 			fi
 		done
 
-		# no vim server currently open in this window.
 		if [ "$#" -eq "0" ]; then
 			tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername" C-m
 		else
 			tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername --remote-silent $@" C-m
 		fi
 	fi
+	#else
+	#	for p in $(vim --serverlist); do
+	#		if [ "$p" -eq "$currentwindowid" ]; then
+	#			$vim_command --servername $vimservername --remote $@
+	#			exit
+	#		fi
+	#	done
+
+	#	# no vim server currently open in this window.
+	#	if [ "$#" -eq "0" ]; then
+	#		tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername" C-m
+	#	else
+	#		tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername --remote-silent $@" C-m
+	#	fi
+	#fi
 
 # otherwise we're not in tmux. So just launch Vim business as usual
 else 
