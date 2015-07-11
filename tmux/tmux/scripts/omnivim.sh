2060518c9d41adf62e7d6e08a940dd472f6cc4eb
andy-lang <langy26@gmail.com>
2015-07-11 16:27:18 +0930

    I think omnivim is finally working..

--- tmux/tmux/scripts/omnivim.sh
+++ tmux/tmux/scripts/omnivim.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 
+#source ~/.bash_aliases
+
+vim_command='gvim -v'
+
 # make sure we're in tmux. If we are...
 if [ -n "$TMUX" ]; then
 	currentpaneid=$(tmux display-message -p "#D")
 	currentwindowid=$(tmux display-message -p "#{window_id}")
-	vimservercount=$(vim --serverlist | wc -l)
+	vimservercount=$($vim_command --serverlist | wc -l)
+	winname=${currentwindowid:1}
+	#echo "WINNAME = "$winname
 
-	echo $currentpaneid, $currentwindowid, $vimservercount
+	#echo $currentpaneid, $currentwindowid, $vimservercount
 	vimservername="$currentwindowid"
+	#echo "vim servername = " $vimservername
 
 	# no current vim servers open. Create a new vim server
 	if [ "$vimservercount" -eq "0" ]; then
 		# if command line parameters passed in, use 'em.
 		if [ "$#" -eq "0" ]; then
-			vim --servername $vimservername
+			tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername" C-m
 		else
-			vim --servername $vimservername --remote-silent $@
+			tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername --remote-silent $@" C-m
 		fi
 	
 	# there is a vim server open in the current window. So use it
 	elif [ "$vimservercount" -eq "1" ]; then
-		vim --servername $vimservername --remote $@
+		$vim_command --servername $vimservername --remote $@
 		##?? TODO switch to pane id of vim
 	
 	# we have multiple vim servers. We want the one that's in the current window.
@@ -29,20 +36,20 @@ if [ -n "$TMUX" ]; then
 	else
 		for p in $(vim --serverlist); do
 			if [ "$p" -eq "$currentwindowid" ]; then
-				vim --servername $vimservername --remote $@
+				$vim_command --servername $vimservername --remote $@
 				exit
 			fi
 		done
 
 		# no vim server currently open in this window.
 		if [ "$#" -eq "0" ]; then
-			vim --servername $vimservername
+			tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername" C-m
 		else
-			vim --servername $vimservername --remote-silent $@
+			tmux send-keys -t "$currentpaneid" "$vim_command --servername $vimservername --remote-silent $@" C-m
 		fi
 	fi
 
 # otherwise we're not in tmux. So just launch Vim business as usual
 else 
-	vim $@
+	$vim_command $@
 fi
