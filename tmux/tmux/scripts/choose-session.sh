584d7e0d23f05dfba8550a1cb21c81f31608c7ce
andy-lang <langy26@gmail.com>
2015-07-17 21:13:14 +0930

    choose-session is now a much more streamlined process

--- tmux/tmux/scripts/choose-session.sh
+++ tmux/tmux/scripts/choose-session.sh
@@ -6,43 +6,42 @@ tmux start-server
 echo "Options: "
 
 declare -a current_sessions
+declare -a script_templates
+i=0
 for	line in $(tmux ls -F '#S'); do
 	#tmux ls -F '#S'
-	echo ${#current_sessions[@]}')' $line
+	echo "$i"')' $line
 	current_sessions+=("$line")
+	let i+=1
 done
+echo "$i"') new session'
+let i+=1
 
-echo ${#current_sessions[@]}') new session'
-echo $((${#current_sessions[@]}+1))') new session from script'
+prevWD=$PWD
+cd ~/.tmux/templates
+for	line in $(ls *.sh); do
+	echo "$i"')' $line '(template)'
+	script_templates+=("$line")
+	let i+=1
+done
+cd $prevWD
 
 read -p "What do you want? " num
 
 if [ $num -lt ${#current_sessions[@]} ]; then
+	# attach to existing session
 	tmux attach-session -t ${current_sessions[$num]}
-elif [ $num -eq ${#current_sessions[@]} ]; then
+elif [ $num -eq $((${#current_sessions[@]})) ]; then
+	# new session, no template
 	read -p "Name for new session: " name
-	#tmux new-session bash -s $name
-	#tmux new bash
 	tmux new-session -s $name
 else
-	declare -a script_templates
-	echo "list of script templates:"
+	decr=$((${#current_sessions[@]}+1))
+	idx=$((num - decr))
 
-	prevWD=$PWD
-	cd ~/.tmux/templates
-	for	line in $(ls *.sh); do
-		#tmux ls -F '#S'
-		echo ${#script_templates[@]}')' $line
-		script_templates+=("$line")
-	done
-	cd $prevWD
-	read -p "Template: " tempnum
-	if [ $tempnum -lt ${#script_templates[@]} ]; then
-		read -p "Name for new session: " name
-		shell_cmd="~/.tmux/templates/"${script_templates[$tempnum]}
-		#chmod +x $shell_cmd
-		tmux new-session -d -s $name
-		tmux send-keys -t $name.0 "$shell_cmd" Enter
-		tmux attach-session -t $name
-	fi
+	read -p "Name for new session: " name
+	shell_cmd="~/.tmux/templates/"${script_templates[$idx]}
+	tmux new-session -d -s $name
+	tmux send-keys -t $name.0 "$shell_cmd" Enter
+	tmux attach-session -t $name
 fi
