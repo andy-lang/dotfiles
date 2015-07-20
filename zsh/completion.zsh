a5a5638f6ff87e669bb9c3a54e378b411340a8c0
andy-lang <langy26@gmail.com>
2015-07-20 14:53:38 +0930

    Added approximate completion to zsh

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -2,7 +2,7 @@
 autoload -Uz compinit
 compinit
 
-zstyle ':completion:*' completer _expand _complete _files _ignored  _correct 
+zstyle ':completion:*' completer _expand _complete _files _ignored  _correct _approximate
 zstyle ':completion:*' insert-tab pending
 setopt noautomenu # turn off tab menu
 
