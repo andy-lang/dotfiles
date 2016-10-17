a3c89fd2e50be51ecb7575dec043bc6c71f6c4af
andy-lang <langy26@gmail.com>
2016-10-18 09:22:44 +1030

    Remove SVN + Git snippets

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -42,27 +42,10 @@ alias vim="python ~/.tmux/scripts/omnivim.py"
 export OMNIVIM_EDITOR=/usr/bin/nvim
 export NVIM_SOCKET_PATH='/home/andrew/'
 
-# Git. Most of the others are covered by OMZ's git plugin
+# Git: alias hub to git if it exists
 if hash hub 2>/dev/null; then
 	alias git=hub # always use Github's nifty plugin
 fi
-alias gpom='git push origin master'
-alias gun='git checkout --' # "git undo"
-alias gap='git add -p'
-
-# aliases for svn
-alias sva='svn add'
-alias svc='svn commit'
-alias svd='svn diff'
-alias svl='svn log'
-alias svm='svn move'
-alias svmk='svn mkdir --parents'
-alias svr='svn revert'
-alias svrr='svn revert --depth=infinity'
-alias svs='svn status'
-alias svu='svn update'
-alias svrm='svn delete'
-alias svrmk='svn delete --keep-local'
 
 # Lastpass
 alias llog='lpass login'
