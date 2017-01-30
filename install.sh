aac8056dd2f470f1c7794a41418ad6a4ec2c64fb
andy-lang <langy26@gmail.com>
2017-01-30 20:52:24 +1030

    Add command line flags to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 cmd="stow"
+while getopts "nv" opt; do
+	case "$opt" in
+		n)
+			cmd="$cmd -n"
+			;;
+		v)
+			cmd="$cmd -v"
+			;;
+	esac
+done
+
+echo "$cmd"
+
 if ! command -v $cmd > /dev/null; then
 	printf "%s is not installed. Please install stow before continuing.\n" "$cmd"
 	exit 1
@@ -8,5 +21,6 @@ fi
 
 declare -a stow_dirs=("bash" "editorconfig" "gdb" "git" "latexmk" "tmux" "vim" "zsh")
 for d in "${stow_dirs[@]}"; do
-	stow "$d"
+	full_cmd="$cmd $d"
+	$($full_cmd)
 done
